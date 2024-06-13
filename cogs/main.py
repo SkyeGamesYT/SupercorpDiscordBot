3b44a1682532b3552c8a198db454bcba059f7770
SkyeGamesYT <113136719+SkyeGamesYT@users.noreply.github.com>
2024-06-13 15:09:38 -0500

    Create main.py

---
+++ cogs/main.py
@@ -0,0 +1,24 @@
+import discord
+from discord import ui, ButtonStyle
+import os
+import asyncio
+import datetime
+from discord import client
+from discord import member
+from discord.utils import get
+from discord.app_commands.commands import describe
+from discord.ext import commands
+import sqlite3
+import random
+import aiohttp
+import roblox
+from roblox import Client, utilities, thumbnails
+from roblox import groups
+from roblox import members
+from roblox.thumbnails import AvatarThumbnailType
+import roblox.thumbnails
+from typing import Optional
+from key_generator.key_generator import generate
+from wonderwords import RandomSentence
+
+
