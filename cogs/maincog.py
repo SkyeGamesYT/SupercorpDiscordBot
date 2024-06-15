96a5d828eb11dbc5614ec0476b9921894b551d97
SkyeGamesYT <113136719+SkyeGamesYT@users.noreply.github.com>
2024-06-15 09:22:25 -0500

    Update and rename main.py to maincog.py

--- cogs/maincog.py
+++ cogs/maincog.py
@@ -21,4 +21,26 @@ from typing import Optional
 from key_generator.key_generator import generate
 from wonderwords import RandomSentence
 
+connection = sqlite3.connect("database.sqlite")
+cursor = connection.cursor()
+rblx = Client("Roblox_Account_Token")
 
+
+class Main(commands.Cog):
+  def __init__(self, bot: commands.Bot):
+    self.bot = bot
+
+
+
+  @commands.command()
+  async def slap(ctx, member: discord.User):
+    await ctx.send(f"{ctx.message.author.mention} slaps {member.mention}!")
+
+  @commands.command()
+  async def echo(self, ctx, *, args):
+    args = str(args)
+    await ctx.send(args)
+
+
+  async def setup(bot: commands.Bot):
+    await bot.add_cog(Main(bot))
